56701ae52a1d9dd8438ecc985698a277a1f622f3
Your Name <you@example.com>
2026-01-27 23:56:27 +0000

    Move middleware to valid location

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,17 +3,15 @@ import { NextResponse } from "next/server";
 
 export default withAuth(
   function middleware(req) {
-    // Return the response as normal
     return NextResponse.next();
   },
   {
     callbacks: {
-      authorized: ({ token }) => !!token, // Only allow if user is logged in
+      authorized: ({ token }) => !!token,
     },
   }
 );
 
-// Protect these routes
 export const config = {
   matcher: [
     "/dashboard/:path*", 
