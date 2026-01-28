7a32d86792e1444152f214d60aa529e311d689ef
Your Name <you@example.com>
2026-01-28 00:00:08 +0000

    Force fix middleware location

--- src/middleware.ts
+++ src/middleware.ts
@@ -13,9 +13,5 @@ export default withAuth(
 );
 
 export const config = {
-  matcher: [
-    "/dashboard/:path*", 
-    "/admin/:path*",
-    "/api/admin/:path*"
-  ],
+  matcher: ["/dashboard/:path*", "/admin/:path*", "/api/admin/:path*"],
 };
